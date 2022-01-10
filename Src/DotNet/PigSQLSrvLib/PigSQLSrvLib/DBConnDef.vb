a4b016dadb3e9ba66250d904bf6e100d1087b9e8
seowphong <seowphong@hotmail.com>
2022-01-10 12:48:47 +0800

    Add PigSQLSrvLib.1.8.12

---
+++ Src/DotNet/PigSQLSrvLib/PigSQLSrvLib/DBConnDef.vb
@@ -0,0 +1,17 @@
+﻿'**********************************
+'* Name: DBConnDef
+'* Author: Seow Phong
+'* License: Copyright (c) 2020 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
+'* Describe: Database connection definition
+'* Home Url: https://www.seowphong.com or https://en.seowphong.com
+'* Version: 1.0
+'* Create Time: 17/10/2021
+'**********************************
+Public Class DBConnDef
+    Inherits PigBaseMini
+    Private Const CLS_VERSION As String = "1.0.1"
+
+    Public Sub New()
+        MyBase.New(CLS_VERSION)
+    End Sub
+End Class
---
+++ Src/DotNet/PigSQLSrvLib/PigSQLSrvLib/DBConnMgr.vb
@@ -0,0 +1,18 @@
+﻿'**********************************
+'* Name: DBConnMgr
+'* Author: Seow Phong
+'* License: Copyright (c) 2020 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
+'* Describe: Database connection management
+'* Home Url: https://www.seowphong.com or https://en.seowphong.com
+'* Version: 1.0
+'* Create Time: 17/10/2021
+'**********************************
+Public Class DBConnMgr
+    Inherits PigBaseMini
+    Private Const CLS_VERSION As String = "1.0.1"
+
+    Public Sub New()
+        MyBase.New(CLS_VERSION)
+    End Sub
+
+End Class
